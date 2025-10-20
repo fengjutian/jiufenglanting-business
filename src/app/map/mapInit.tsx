eaa36f4c36d08fa779b4a7e3d88d294acf6ebf3f
fengjutian <fjutian@foxmail.com>
2025-10-20 16:02:17 +0800

    refactor(map): 提取地图配置为常量并优化代码格式
    将地图中心点和API key提取为模块级常量，提高代码可维护性
    优化代码缩进和格式，提升可读性

--- src/app/map/mapInit.tsx
+++ src/app/map/mapInit.tsx
@@ -1,33 +1,34 @@
 import React, { useEffect } from 'react';
 import AMapLoader from '@amap/amap-jsapi-loader';
+
+const position = [118.881076,31.960958]
+const key = '5131350db8ad49230fd4c7f3cab4f1d8'
  
 const MapInit = () => {
-    useEffect(() => {
-        AMapLoader.load({
-            "key": "5131350db8ad49230fd4c7f3cab4f1d8",   // 申请好的Web端开发者Key，首次调用 load 时必填
-            "version": "2.0",   // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
-            "plugins": []  //插件列表
-        }).then((AMap) => {
-            let amap = new AMap.Map('mapContainer', { // mapcontainer为容器的id
-                zoom: 15, //初始化地图层级
-                center: [118.881121,31.960383] //初始化地图中心点
-            });
-            // 标记
-            let marker = new AMap.Marker({
-                position: [118.881121,31.960383] // 基点位置
-            });
-            // 地图添加标记
-            amap.add(marker);
-        }).catch(e => {
-            console.log(e);
-        })
- 
- 
-    }, []);
- 
-    return (
-      <div id="mapContainer" style={{ width: '100vw', height: '100vh' }}></div>
-    );
+  useEffect(() => {
+    AMapLoader.load({
+      "key": key,   // 申请好的Web端开发者Key，首次调用 load 时必填
+      "version": "2.0",   // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+      "plugins": []  //插件列表，如比例尺'AMap.Scale'等
+    }).then((AMap) => {
+      let amap = new AMap.Map('mapContainer', { // mapcontainer为容器的id
+        zoom: 15, //初始化地图层级
+        center: position //初始化地图中心点
+      });
+      // 标记
+      let marker = new AMap.Marker({
+        position: position // 基点位置
+      });
+      // 地图添加标记
+      amap.add(marker);
+    }).catch(e => {
+        console.log(e);
+    })
+  }, []);
+
+  return (
+    <div id="mapContainer" style={{ width: '100vw', height: '100vh' }}></div>
+  );
 };
  
 export default MapInit;
