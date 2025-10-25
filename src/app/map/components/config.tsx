c3bf60c60029ed0e833e8d9957ff21982086ce30
fengjutian <fjutian@foxmail.com>
2025-10-25 14:51:18 +0800

    refactor(map): 重构配置组件中的主题选择逻辑
    将硬编码的主题选项替换为动态渲染，提高代码可维护性

--- src/app/map/components/config.tsx
+++ src/app/map/components/config.tsx
@@ -37,33 +37,18 @@ export const Config: React.FC<configType> = (props: configType) => {
 
 					<Box maxWidth="600px">
 						<RadioCards.Root defaultValue="1" columns={{ initial: "1", sm: "3" }}>
-							<RadioCards.Item value="1">
-								<Flex direction="column" width="100%">
-									<Text weight="bold">8-core CPU</Text>
-									<Text>32 GB RAM</Text>
-								</Flex>
-							</RadioCards.Item>
-							<RadioCards.Item value="2">
-								<Flex direction="column" width="100%">
-									<Text weight="bold">6-core CPU</Text>
-									<Text>24 GB RAM</Text>
-								</Flex>
-							</RadioCards.Item>
-							<RadioCards.Item value="3">
-								<Flex direction="column" width="100%">
-									<Text weight="bold">4-core CPU</Text>
-									<Text>16 GB RAM</Text>
-								</Flex>
-							</RadioCards.Item>
+							{
+								mapTheme.map((item: any) => (
+									<RadioCards.Item key={item.label} value={item.theme}>
+										<Flex direction="column" width="100%">
+											<Text weight="bold">{item.name}</Text>
+											<Text>{item.description}</Text>
+										</Flex>
+									</RadioCards.Item>
+								))
+							}
 						</RadioCards.Root>
 					</Box>
-
-	
-						{mapTheme.map((item: any) => (
-							<label key={item.label} value={item.theme}>
-								{item.name}
-							</label>
-						))}
 					
 				</DrawerContent>
 			</Drawer>
