43036211679c439a05808e1141dec96c1d9fadb3
fengjutian <fjutian@foxmail.com>
2025-11-14 22:55:09 +0800

    feat(业务类型): 更新业务类型枚举并添加新店铺数据
    - 将'面包店'类型重命名为'糕点'并添加'肯德基'类型
    - 新增肯德基门店数据并修正上访天云门诊的类型
    - 添加孙北北面包店数据，使用新的'糕点'类型

--- scripts/business.ts
+++ scripts/business.ts
@@ -76,14 +76,38 @@ export const raw: BusinessSeedEntry[] = [
   },
   {
     id: 6,
+    name: '肯德基(南京上坊店)',
+    email: '',
+    address: '南京市江宁区东山街道文靖东路333号1幢104室一层',
+    type: typeList.小诊所,
+    contact: '',
+    rating: null,
+    latitude: 31.968904,
+    longitude: 118.876375,
+    otherInfo: null
+  },
+  {
+    id: 7,
     name: '上访天云门诊',
     email: '',
     address: '南京市江宁区东山街道祈泽佳苑9幢一楼102-105室',
-    type: typeList.小诊所,
+    type: typeList.肯德基,
     contact: '',
     rating: null,
     latitude: 31.9689,
     longitude: 118.876378,
+    otherInfo: null
+  },
+  {
+    id: 8,
+    name: '孙北北面包(江宁金茂悦店)',
+    email: '',
+    address: '南京市江宁区瑞宁路188号',
+    type: typeList.糕点,
+    contact: '15195808604',
+    rating: null,
+    latitude: 31.960703,
+    longitude: 118.888914,
     otherInfo: null
   }
 ]
