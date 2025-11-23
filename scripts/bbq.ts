86293e2d5472098a7ddc64a97d72b1254d84119d
fengjutian <fjutian@foxmail.com>
2025-11-23 13:12:20 +0800

    feat: 添加肥肥龙虾烧烤到商家列表

--- scripts/bbq.ts
+++ scripts/bbq.ts
@@ -16,4 +16,18 @@ export const bbqList: BusinessSeedEntry[] = [
     imageBase64: null,
     description: null
   },
+  {
+    id: 'bbq-2',
+    name: '肥肥龙虾烧烤',
+    email: '',
+    address: '南京市江宁区天宁路与园中路交叉口北80米',
+    type: typeList.烧烤,
+    contact: '',
+    rating: null,
+    latitude: 31.956006,
+    longitude: 118.885404,
+    otherInfo: null,
+    imageBase64: null,
+    description: null
+  },
 ]
